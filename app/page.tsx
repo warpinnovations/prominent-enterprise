d0177b6699a5284b538d4118289c3b7e912b2959
Casey Rhyss Capacio <capaciocasey@gmail.com>
2026-02-05 15:14:24 +0800

    refactor: remove LogoCloud component from Home page to streamline layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import { Navbar } from "@/components/Navbar";
 import { Hero } from "@/components/Hero";
-import { LogoCloud } from "@/components/LogoCloud";
 import { Features } from "@/components/Features";
 import { Pricing } from "@/components/Pricing";
 import { CTA, Footer } from "@/components/Footer";
@@ -10,7 +9,6 @@ export default function Home() {
     <main className="min-h-screen mesh-gradient">
       <Navbar />
       <Hero />
-      <LogoCloud />
       <Features />
       <Pricing />
       <CTA />
